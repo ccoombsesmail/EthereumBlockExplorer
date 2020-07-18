5784647811c75ec6ebe6dd7bf5efce907f2a92b4
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-07-18 01:46:09 -0700

    Add mongo options

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	// "EthereumBlockExplorer/routes"
 	// "EthereumBlockExplorer/websockets"
 	"go.mongodb.org/mongo-driver/mongo"
-	// "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"os"
 	"EthereumBlockExplorer/config"
 )

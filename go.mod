a7da2e53aa370548b4199adcc38a471d89ea0235
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-07-18 01:40:01 -0700

    Test heroku deploy

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module EthereumBlockExplorer
 
-+heroku goVersion go1.14
+go 1.14
 
 require (
 	github.com/btcsuite/btcd v0.20.1-beta // indirect
--- main.go
+++ main.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"log"
-	"time"
+	// "time"
 	"net/http"
-	"EthereumBlockExplorer/routes"
-	"EthereumBlockExplorer/websockets"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	// "EthereumBlockExplorer/routes"
+	// "EthereumBlockExplorer/websockets"
+	// "go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
 	"os"
-	"EthereumBlockExplorer/config"
+	// "EthereumBlockExplorer/config"
 )
 
 
 
 func main() {
 
-	mongoClient, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://user1:" + config.GetKey() + "@cluster0.4mnma.mongodb.net/<blockHistoryDB>?retryWrites=true&w=majority"))
-	if err != nil {
-			log.Fatal(err)
-	}
+	// mongoClient, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://user1:" + config.GetKey() + "@cluster0.4mnma.mongodb.net/<blockHistoryDB>?retryWrites=true&w=majority"))
+	// if err != nil {
+	// 		log.Fatal(err)
+	// }
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = mongoClient.Connect(ctx)
-	if err != nil {
-			log.Fatal(err)
-	}
-	defer mongoClient.Disconnect(ctx)
+	// ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	// err = mongoClient.Connect(ctx)
+	// if err != nil {
+	// 		log.Fatal(err)
+	// }
+	// defer mongoClient.Disconnect(ctx)
 
-	db := mongoClient.Database("blockHistoryDB")
-	blocksCollection := db.Collection("blocks")
-	transactionsCollection := db.Collection("transactions")
+	// db := mongoClient.Database("blockHistoryDB")
+	// blocksCollection := db.Collection("blocks")
+	// transactionsCollection := db.Collection("transactions")
 
   port := os.Getenv("PORT")
  	if port == "" {
-                port = "5000"
-                log.Printf("Defaulting to port %s", port)
-        }
-	http.HandleFunc("/ws", websockets.ServeWs)
-	routes.SetupBlockRoutes(mongoClient)
-	routes.SetupTransactionRoutes(mongoClient)
-	go websockets.SubToBlockHeader(blocksCollection, transactionsCollection)
-	fs := http.FileServer(http.Dir("./build"))
+			port = "5000"
+			log.Printf("Defaulting to port %s", port)
+    }
+	// http.HandleFunc("/ws", websockets.ServeWs)
+	// routes.SetupBlockRoutes(mongoClient)
+	// routes.SetupTransactionRoutes(mongoClient)
+	// go websockets.SubToBlockHeader(blocksCollection, transactionsCollection)
+	// fs := http.FileServer(http.Dir("./build"))
 	http.Handle("/", fs)
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))

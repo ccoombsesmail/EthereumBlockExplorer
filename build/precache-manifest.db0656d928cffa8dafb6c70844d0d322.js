321a21760a2c13a9cb57021112fb02c8b9e214be
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-07-18 19:44:07 -0700

    Update frontend build

---
+++ build/precache-manifest.db0656d928cffa8dafb6c70844d0d322.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c23f306b8961c369764641fcfd40e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10fafa69707d1e629135",
+    "url": "/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "d22474fe2c2a23d253ef",
+    "url": "/static/css/main.de3e97d2.chunk.css"
+  },
+  {
+    "revision": "10fafa69707d1e629135",
+    "url": "/static/js/2.d91c6a96.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.d91c6a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22474fe2c2a23d253ef",
+    "url": "/static/js/main.28eed06d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9c3a4ed1a3b846fd4a8532a29f030d1.js"
+  "/precache-manifest.db0656d928cffa8dafb6c70844d0d322.js"
 );
 
 self.addEventListener('message', (event) => {
